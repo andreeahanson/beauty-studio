7f704c266b61db6e96a654bcf9c7f162d8eb3d70
Andreea Hanson <andreea.hanson7@gmail.com>
2019-07-25 10:35:57 -0600

    Add NavBar file and function

---
+++ src/components/NavBar.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './NavBar.css';
+
+const NavBar = () => {
+  return (
+    <nav>
+      <p className="nav-word">Blush</p>
+      <p className="nav-word">Bronzer</p>
+      <p className="nav-word">Eyebrow</p>
+      <p className="nav-word">Eyeliner</p>
+      <p className="nav-word">Eyeshadow</p>
+      <p className="nav-word">Foundation</p>
+      <p className="nav-word">Lip Liner</p>
+      <p className="nav-word">Lipstick</p>
+      <p className="nav-word">Mascara</p>
+      <p className="nav-word" >Nail polish</p>
+      <p className="nav-word" className='red'>Loves list</p>
+    </nav>
+  )
+}
+
+export default NavBar;
