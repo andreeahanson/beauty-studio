d57924e24f59aba381cfbb6c4e32130dcad46180
Andreea Hanson <andreea.hanson7@gmail.com>
2019-07-25 10:26:52 -0600

    Add ProductCollection file and function

---
+++ src/components/ProductCollection.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ProductCard from '../containers/ProductCard';
+
+
+const ProductCollection = (products) => {
+  console.log(products)
+  const allProducts = products.products.map(product => {
+    if(product.price != 0){
+    return (
+    <ProductCard 
+    id={product.created_at}
+    key={product.created_at}
+    brand={product.brand} 
+    name={product.name} 
+    description={product.description}
+    price={product.price} 
+    image={product.image_link} 
+    colors={product.product_colors}
+    type={product.product_type}
+    tagList={product.tag_list}
+    />)
+    }
+  })
+  return allProducts
+}
+
+export default ProductCollection;
