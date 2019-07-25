95cd3471ac1d6642931957328b99410080f6ff30
Andreea Hanson <andreea.hanson7@gmail.com>
2019-07-25 10:19:49 -0600

    Setup App

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { fetchMakeup } from '../apiCalls';
+import { connect } from 'react-redux';
+import { loadProducts } from '../actions';
+import ProductCollection from '../components/ProductCollection';
+import { sampleProducts } from '../sampleCall';
+import NavBar from '../components/NavBar';
+import Banner from '../components/Banner';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {}
+  }
+  
+  componentDidMount = async () => {
+    // const makeup = await fetchMakeup()
+    // await console.log(makeup)
+    // await this.props.loadProducts(makeup)
+    
+  }
+  
+  render(){
+    let products = sampleProducts;
+    this.props.loadProducts(products)
+    console.log(products)
+    // const {products} = this.props
+    return(
+      <section className="App">
+            <Banner />
+            <NavBar />
+          <section className="product-collection">
+            <ProductCollection products={products} />
+          </section>
+      </section>
+    )
+  }
 }
 
-export default App;
+const mapStateToProps = state => ({
+  products: state.products
+})
+
+const mapDispatchToProps = dispatch => ({
+  loadProducts: products => dispatch(loadProducts(products))
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
