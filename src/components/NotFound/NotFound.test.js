cc4ee5fc3d401438d444c275b6967456bbc72171
Andreea Hanson <andreea.hanson7@gmail.com>
2019-07-28 18:09:19 -0600

    Add not found test snapshot

---
+++ src/components/NotFound/NotFound.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import NotFound from './NotFound';
+import { shallow } from 'enzyme';
+
+describe('NotFound', () => {
+  it('should match the snapshot', () => {
+    let wrapper=shallow(<NotFound />)
+
+    expect(wrapper).toMatchSnapshot();
+  })
+}) 
