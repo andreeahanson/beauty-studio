65f8b52da3d28c68b495020c3949d841bb83233d
Andreea Hanson <andreea.hanson7@gmail.com>
2019-07-30 01:38:40 -0600

    Align not found message

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -5,8 +5,7 @@ import './NotFound.css'
 const NotFound = () => {
   return (
     <>
-    <h2 className='not-found'>Page not found or loading</h2>
-    <h2 className='not-found'>Go back to <Link className='not-found' to='/'>home</Link> page</h2>
+    <h3 className='not-found'>Page not found. Go back to <Link className='not-found' to='/'>home</Link> page</h3>
     </>
   )
 }
