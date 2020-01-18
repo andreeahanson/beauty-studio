d4625f5a26d66d196e55fe6cd3f30ec23aac3be5
Andreea Hanson <andreea.hanson7@gmail.com>
2020-01-17 20:48:55 -0700

    Align  items on left side of DetailPage

--- src/components/NotesForm/NotesForm.js
+++ src/components/NotesForm/NotesForm.js
@@ -4,6 +4,7 @@ import "./NotesForm.css";
 const NotesForm = () => {
   return(
     <form>
+      <h5>THIS SECTION IS UNDER CONSTRUCTION ... </h5>
       <h4>My notes: </h4>
       <input type="text" />
       <button>Add my note</button>
